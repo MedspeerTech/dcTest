010251ded6931a882788ecd4fcb60229effef3a3
Vishnu Manohar <vishnumanohar@outlook.com>
2016-01-24 09:15:47 +0530

    First commit

---
+++ src/main/java/dc/test/Application.java
@@ -0,0 +1,25 @@
+package dc.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableWebMvc
+@ComponentScan(basePackages = {"dc.test"})
+public class Application extends SpringBootServletInitializer {
+    private static Logger log = LogManager.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.error("---------------------error for testing ");
+        System.out.println("Running test");
+    }
+}
